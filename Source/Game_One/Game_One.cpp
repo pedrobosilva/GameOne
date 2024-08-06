c93205b91531ce21cb8e34fc7586d1a8c88712fe
DESKTOP-G0DJ3L5\pedro <pedro.silva@ironstudiosinc.com>
2024-08-06 10:25:02 -0300

    RepoStart

---
+++ Source/Game_One/Game_One.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Game_One.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Game_One, "Game_One" );
